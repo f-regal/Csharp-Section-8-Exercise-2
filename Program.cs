308e1a8204e43eba56f5a60c6d1579b201ac19cd
Faysal Regal <faysalregal@gmail.com>
2022-08-15 20:29:51 +0100

    Started exercise 2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Exercise 2
+//Write a program and ask the user to enter a few numbers separated by a hyphen. 
+//If the user simply presses Enter, without supplying an input, exit immediately; 
+//otherwise, check to see if there are duplicates. 
+//If so, display "Duplicate" on the console.
+
+Console.WriteLine("Please enter a few numbers separated by a hyphen");
+
+var userInput = Console.ReadLine();
+
+if(String.IsNullOrEmpty(userInput))
+{
+    //Environment.Exit(0);
+    Console.WriteLine(userInput);
+}
