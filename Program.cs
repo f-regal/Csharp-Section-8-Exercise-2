7ed4d4e75b280a42c10ba9afa1883d8f81c2dafd
Faysal Regal <faysalregal@gmail.com>
2022-08-16 20:32:34 +0100

    Updated exercise 2

--- Program.cs
+++ Program.cs
@@ -4,12 +4,76 @@
 //otherwise, check to see if there are duplicates. 
 //If so, display "Duplicate" on the console.
 
-Console.WriteLine("Please enter a few numbers separated by a hyphen");
 
-var userInput = Console.ReadLine();
 
-if(String.IsNullOrEmpty(userInput))
+
+
+
+namespace CsharpFundamentals 
 {
-    //Environment.Exit(0);
-    Console.WriteLine(userInput);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a few numbers separated by a hyphen");
+            var userInput = Console.ReadLine();
+
+            if (String.IsNullOrEmpty(userInput))
+            {
+                Environment.Exit(0);
+            } else
+            {
+                var splitInput = userInput.Split('-');
+                var duplicates = false;
+                List<int> inputArr = new List<int>();
+
+                //[1,2,3] i = 0,1,2
+                //[1,2,3] j = 1,2
+
+                //i loops 0,1,2,3
+                //j loops 1,2,3,4
+
+                //first loop
+                //i = 0, val = 1 & j = 1, val = 1 (Not Dup)
+
+                //i = 0, val = 1 & j = 2, val = 3 (Not Dup)
+
+
+                //second loop
+                //i = 1, val = 1 & j = 1, val = 2(Not Dup)
+
+                //i = 1, val = 2 & j = 2, val = 3(Not Dup)
+
+                //i = 1, val = 3 & j = 3, val = 1(Not Dup)
+
+
+                for (int i = 0; i < splitInput.Length; i++) {
+                    for (int j = 0; j < splitInput.Length - 1; j++)
+                    {
+                        if (int.Parse(splitInput[i]) != int.Parse(splitInput[j + 1]))
+                            {
+                            duplicates = false;
+                        } else
+                        {
+                            duplicates = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (duplicates)
+                {
+                    Console.WriteLine("Duplicates");
+                } else
+                {
+                    Console.WriteLine("No Duplicates");
+                }
+
+                
+
+               
+            }
+        }
+    }
+
 }
